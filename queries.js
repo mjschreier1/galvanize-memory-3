94db98a8ee46810d9a3402a52671ceecd411bdc7
Michael Schreier <mjschreier1@gmail.com>
2018-02-26 10:56:42 -0700

    query functions created

--- queries.js
+++ queries.js
@@ -2,13 +2,19 @@ const database = require("./database-connection");
 
 module.exports = {
     list(){
+        return database.select("*").from("games");
     },
     read(id){
+        return database.select("*").from("games").where("id", id).first();
     },
     create(game){
+        return database("games").create(game, "*");
     },
     update(id, game){
+        return database.select("*").from("games").where("id", id)
+            .update("game", game, "*");
     },
     delete(id){
+        return database.select("*").from("games").where("id", id).del();
     }
 };

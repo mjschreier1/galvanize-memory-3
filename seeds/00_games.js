4c6e38017a5e9e63cb2f64c1eb9cc60899836c12
Michael Schreier <mjschreier1@gmail.com>
2018-02-26 10:49:06 -0700

    seeds run

---
+++ seeds/00_games.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex, Promise) {
+  return knex("games").del()
+    .then(function () {
+      return knex("games").insert([
+        { id: 1, name: "Breath of the Wild", developer: "Nintendo", rating: 8.5 },
+        { id: 2, name: "Fez", developer: "Polytron", rating: 7.4 },
+        { id: 3, name: "Fez", developer: "Demruth", rating: 8.1 },
+      ]);
+    })
+    .then(() => {
+      knex.raw("ALTER SEQUENCE games_id_seq RESTART WITH 4")
+    });
+};
